afc676cc9bb2e5fb99645ab61ff5c5ea1296a97f
ashraful2430 <www.savage582@gmail.com>
2024-06-27 19:27:52 +0600

    created Login class for reusable

---
+++ cypress/e2e/pages/Login.js
@@ -0,0 +1,32 @@
+class Login {
+  //   login to the website
+  loginToTheWebsite() {
+    cy.visit("https://staging.biyekorun.us/");
+
+    // Clicking on the login button
+    cy.get(".flex-gap-15 > .primary-btn-v3").click();
+
+    // Getting the email field and inputting the email
+    cy.get(
+      "body > div:nth-child(16) > div > div > div > div > div.loginComp__wrapper--left.box-shadow.flex.flex-column.justify-center.align-center > div.mantine-InputWrapper-root.mantine-TextInput-root.mantine-1sq88d3 > div > input"
+    ).type("ashrafulislamashik960@gmail.com");
+
+    // Getting the password field and typing the password
+    cy.get(
+      "body > div:nth-child(16) > div > div > div > div > div.loginComp__wrapper--left.box-shadow.flex.flex-column.justify-center.align-center > div:nth-child(5) > div > div > div.mantine-PasswordInput-input.mantine-Input-input.mantine-PasswordInput-input.mantine-a7hhxl > input"
+    ).type("Ashik@2430");
+
+    // Clicking on the sign in button
+    cy.get(
+      "body > div:nth-child(16) > div > div > div > div > div.loginComp__wrapper--left.box-shadow.flex.flex-column.justify-center.align-center > button > div"
+    ).click();
+
+    // Ensure the modal is fully loaded
+    cy.wait(1000); // Wait for 1 second or adjust as needed
+
+    // Ensure the close button is visible and click on it
+    cy.get(".btn-fill").scrollIntoView().should("be.visible").click();
+  }
+}
+
+export default Login;
