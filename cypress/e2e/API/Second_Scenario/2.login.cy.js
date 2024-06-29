404f309ffec864716594f9083b27c89446089030
ashraful2430 <www.savage582@gmail.com>
2024-06-29 21:34:42 +0600

    runned single invite api

--- cypress/e2e/API/Second_Scenario/2.login.cy.js
+++ cypress/e2e/API/Second_Scenario/2.login.cy.js
@@ -1,3 +1,18 @@
 describe("Login user", () => {
-  it("As a groom/bride, I should be able to login to the website", () => {});
+  it("As a groom/bride, I should be able to login to the website", () => {
+    cy.request({
+      method: "POST",
+      url: "/api/user/login",
+      body: {
+        email: "ashrafulislamashik960@gmail.com",
+        password: "Ashik@2430",
+      },
+    }).then((res) => {
+      expect(res.status).to.equal(200);
+      const accessToken = res.body.token.accessToken;
+      cy.writeFile("cypress/fixtures/loginToken.json", {
+        accessToken: accessToken,
+      });
+    });
+  });
 });
--- cypress/e2e/API/Second_Scenario/3.singleInvite.cy.js
+++ cypress/e2e/API/Second_Scenario/3.singleInvite.cy.js
@@ -1,9 +1,31 @@
 describe("Friend request invitation", () => {
+  let accessToken;
+  let newUserId;
+  before(() => {
+    cy.readFile("cypress/fixtures/loginToken.json").then((info) => {
+      accessToken = info.accessToken;
+    });
+    cy.readFile("cypress/fixtures/newUserData.json").then((info) => {
+      newUserId = info.newUserId;
+    });
+  });
   it("As a groom/bride, I should be able to send friend request to another user", () => {
     cy.request({
       method: "POST",
       url: "/api/user/single-invite",
-      headers: { "Accept-Language": "en-us" },
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: {
+        recipient: `${newUserId}`,
+      },
+    }).then((res) => {
+      console.log(res.body);
+      expect(res.message).to.equal("Succeed");
+      const updatedFriend = res.body.data._id;
+      cy.writeFile("cypress/fixtures/updatedFriend.json", {
+        updatedFriend: updatedFriend,
+      });
     });
   });
 });
